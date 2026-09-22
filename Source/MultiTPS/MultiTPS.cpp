92cf172228a44419eb0813fd363fcb3b4d2ba0cd
wuda1234563 <1159328042@qq.com>
2026-09-22 13:53:20 +0800

    chore(project): initialize Unreal project repository

---
+++ Source/MultiTPS/MultiTPS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MultiTPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiTPS, "MultiTPS" );
